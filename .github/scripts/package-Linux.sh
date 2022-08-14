6ba519eca4e22213693426e12f68c51693341812
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2022-08-14 11:40:29 +0200

    Added automatic build system

---
+++ .github/scripts/package-Linux.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cp -r External pd-hvcc-Linux
+cp README.md pd-hvcc-Linux/README.md
+cp LICENSE pd-hvcc-Linux/LICENSE.txt
+
+
---
+++ .github/scripts/package-macOS.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cp -r External pd-hvcc-MacOS
+cp README.md pd-hvcc-MacOS/README.md
+cp LICENSE pd-hvcc-MacOS/LICENSE.txt
+
+
